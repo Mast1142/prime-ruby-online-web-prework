d57497c19175745cfba666083e23ca5499c7e0ec
Samuel W Tong <ssamt1142@gmail.com>
2019-02-22 04:12:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
+require "pry"
 def prime?(n)
   if n <= 0
     return false
   end
-    for i in (1..n.to_i) do
-    if (n.to_i % i == 0)
+    for i in (1..n.to_a) do
+      binding.pry
+    if (n % i == 0)
       return true
     else
       return false
