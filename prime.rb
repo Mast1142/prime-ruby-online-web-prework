478bd8188d436202535b686e33e8048c0f32ff87
Samuel W Tong <ssamt1142@gmail.com>
2019-02-22 04:31:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,20 @@
 require "pry"
 def prime?(n)
+  prime = true
   if n <= 1
     return false
-  end
+  elsif n == 2
+    return prime
+  else  
     for i in (2..n) do
       binding.pry
-    if (n % i == 0)
-    prime = true
-    else
-    prime = false
+      if (n % i == 0)
+        prime = true
+      else
+        prime = false
+      end
     end
   end
+
   prime
 end
